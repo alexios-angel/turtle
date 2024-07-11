711ed22390b59d9deec47c40c75010bc13e1ef3a
Alexios Angel <aangeletakis@gmail.com>
2024-07-11 18:24:37 -0400

    Major update to the token regex; Added named capture groups to the token regex and reorginaized the regex

--- src/lexicalize.cpp
+++ src/lexicalize.cpp
@@ -16,30 +16,36 @@
 
 // clang-format off
 /*
-Paste into regex100.com
-Replace regex comments with )"${1}R"( by using (\(\?#([^)]*)\))|^
+Paste into regex101.com
+Replace regex comments with )"${2}R"( by using (\(\?#([^)]*)\))|^
 
-[a-zA-Z]{-1,2}?(("|')(\2{2})?)((?:[^\\"]|\\.|\\)*\1)?|(?#   //capture strings - check later on if string
-                                                           //                  prefix is valid and the string terminates
-)(#[^\r\n]*)|(?#                                           //capture comments
-)([\n\r][ \t]*)|(?#                                        //capture newlines
-)(\\[^\r\n]*)|(?#                                          //capture \TheBackslashAndAnythingAfterIt
-)((?#
-    )(\.{2})|(?#                                           //capture ...
-    )(->)|(?#                                              //capture ->
+(?<string>(?<complete_quotes>(?<quote_type>"|')(\g{quote_type}{2})?)(?:(?:[^\\"]|\\.|\\)*\g{complete_quotes})?)|(?#
+                                                           //capture strings
+                                                           // prefix is valid and the string terminates
+)(?<comment>#[^\r\n]*)|(?#                                 //capture comments
+)(?<newline>[\n\r][ \t]*)|(?#                              //capture newlines
+)(?<backslash>\\[^\r\n]*)|(?#                              //capture \TheBackslashAndAnythingAfterIt
+)(?<arithmetic>(?#
+    )(?:[!%&*+\-<=>@\/\\^|:]=?)|(?#                        //capture 1 character operators
+    )(?:[<>*\/]{1,2}=?)(?#                                 //capture 2-3 character operators
+))|(?<data>(?#
                                                            //fucking floating point numbers
-    )(\d[\d_]*\.[\d_]*\d[\d_]*[eE]-?[\d_]*)|(?#            //capture exponential floating point literals
-    )(\d[\d_]*\.[\d_]*\d[\d_]*[\w]*)|(?#                   //capture floating point literals -> \d.\d [suffix]
-    )(\d[\d_]*\.[eE]-?[\d_]*)|(?#                          //capture exponential floating point literals
-    )(\d[\d_]*\.\w*)|(?#                                   //capture floating point literals -> \d.   [suffix]
-    )(\.\d[\d_]*[eE]-?[\d_]*)|(?#                          //capture exponential floating point literals
-    )(\.\d[\d_]*\w*)|(?#                                   //capture floating point literals ->   .\d [suffix]
-    )(\d[\d_]*[eE]-?[\d_]*)|(?#                            //capture exponential literals
-    )([<>*\/]{1})=?|(?#                                    //capture 2-3 character operators
-    )([!%&*+\-<=>@\/\\^|:]=)(?#                            //capture 1 caracter operators
+    )(?:\d[\d_]*\.[\d_]*\d[\d_]*[eE]-?[\d_]*)|(?#          //capture exponential floating point literals
+    )(?:\d[\d_]*\.[\d_]*\d[\d_]*[\w]*)|(?#                 //capture floating point literals -> \d.\d [suffix]
+    )(?:\d[\d_]*\.[eE]-?[\d_]*)|(?#                        //capture exponential floating point literals
+    )(?:\d[\d_]*\.\w*)|(?#                                 //capture floating point literals -> \d.   [suffix]
+    )(?:\.\d[\d_]*[eE]-?[\d_]*)|(?#                        //capture exponential floating point literals
+    )(?:\.\d[\d_]*\w*)|(?#                                 //capture floating point literals ->   .\d [suffix]
+    )(?:\d[\d_]*[eE]-?[\d_]*)|(?#                          //capture exponential literals
+    )(?:\d+)(?#
 ))|(?#
-)([!-\/:-@\[-^{-~]|[^\s!-\/:-@\[-^{-~]+)|(?#               //capture anything else
-)(\s+)                                                     //capture whitespace in order to keep track of position with ctre
+)(?<deliminar>(?#
+    )\.{2}|(?#                                             //capture ...
+    )(?:->)|(?#                                            //capture ->
+    )[!-\/:-@\[-^{-~](?#
+))|(?#
+)(?<identifier>[^\s!-\/:-@\[-^{-~]+)|(?#                   //capture anything else
+)(?<whitespace>\s+)(?#                                     //capture whitespace in order to keep track of position with ctre)
 */
         // clang-format off
         //when rEgEX is A LaNGUAgE
